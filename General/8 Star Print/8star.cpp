ef1096e7babc694d85a2715291cb7913d9c746f6
Manish Singh <singhmanish.personal@gmail.com>
2024-08-20 10:44:57 +0530

    Code for 8 star

---
+++ General/8 Star Print/8star.cpp
@@ -0,0 +1,50 @@
+/*clear */
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int row;
+cout<<"Enter the number of rows: ";
+cin>>row;
+cout<<endl<<endl;
+for(int i=1;i<=row;i++){
+
+    if(i==1 || i==row){
+        cout<<" ";
+        for(int j=1;j<=row-2;j++){
+            cout<<"*";
+        }
+        cout<<" "<<endl;
+    }else{
+        cout<<"*";
+        for(int j=1;j<=row-2;j++){
+            cout<<" ";
+        }
+        cout<<"*"<<endl;
+    }
+}
+
+for(int i=1;i<=row-1;i++){
+   if(i==row-1){
+        cout<<" ";
+        for(int j=1;j<=row-2;j++){
+            cout<<"*";
+        }
+        cout<<" "<<endl;
+   }else{
+        cout<<"*";
+        for(int j=1;j<=row-2;j++){
+            cout<<" ";
+        }
+        cout<<"*"<<endl;    
+   }
+
+
+}
+
+return 0;
+
+}
---
+++ General/8 Star Print/Readme..md
@@ -0,0 +1,14 @@
+Print an 8  in star after taing the number  of rows from the user
+
+Enter the number of rows: 5
+
+
+ *** 
+*   *
+*   *
+*   *
+ *** 
+*   *
+*   *
+*   *
+ ***
