27e0654103d9afe511415845ad4adaafee83cc81
Manish Singh <singhmanish.personal@gmail.com>
2025-07-29 00:20:16 +0530

    LC3

---
+++ LeetCode/Sliding Window/LC3_ Longest_Substring_Without_Repeating_Characters/Solution.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int maxLength = 0;
+        int stringLength=s.length();
+        HashSet<Character> tracking = new HashSet<Character>();
+
+        int leftPointer = 0;
+
+        for(int rightPointer=0; rightPointer<stringLength;rightPointer++){
+
+            while(tracking.contains(s.charAt(rightPointer))){
+                tracking.remove(s.charAt(leftPointer));
+                leftPointer++;
+            }
+            tracking.add(s.charAt(rightPointer));
+            maxLength=(rightPointer-leftPointer+1>maxLength)?rightPointer-leftPointer+1:maxLength;
+        }
+
+        return maxLength;
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().lengthOfLongestSubstring("pwwkew"));
+    }
+}
